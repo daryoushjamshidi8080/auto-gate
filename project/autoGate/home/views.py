6d598d242e238c00b68ba114527fba1d018233cf
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-05-23 18:55:33 +0330

    create home page and set configuration

---
+++ project/autoGate/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+
+
+
+class home(View):
+    def get(self, request):
+        return render(request, "home/index.html")
+    def post(self, request):
+        pass
